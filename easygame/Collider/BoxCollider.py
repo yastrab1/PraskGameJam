f85b71cc9bc5f590d0661407ee3f1d6b39d97d9b
IAmBigProOrMaybeNot <lipka.luki@gmail.com>
2024-01-11 01:35:44 +0100

    Moved functionality

--- easygame/Collider/BoxCollider.py
+++ easygame/Collider/BoxCollider.py
@@ -1,4 +1,5 @@
 from easygame.Collider.AbstractCollider import AbstractCollider, Collision
+from easygame.Renderer.Face import Face
 from easygame.Renderer.Raycaster import Ray
 from easygame.vector import Vector3
 
@@ -16,33 +17,21 @@ class BoxCollider(AbstractCollider):
             [corner.x+sideLengths.x,corner.y+sideLengths.y,corner.z+sideLengths.z]
         ]
         self.faces = [
-            [self.vertices[0],self.vertices[1],self.vertices[2],self.vertices[3]],
-            [self.vertices[3],self.vertices[2],self.vertices[7],self.vertices[6]],
-            [self.vertices[6],self.vertices[7],self.vertices[5],self.vertices[4]],
-            [self.vertices[4],self.vertices[5],self.vertices[1],self.vertices[0]],
-            [self.vertices[1],self.vertices[5],self.vertices[7],self.vertices[2]],
-            [self.vertices[4],self.vertices[0],self.vertices[3],self.vertices[6]]
+            Face([self.vertices[0],self.vertices[1],self.vertices[2],self.vertices[3]]),
+            Face([self.vertices[3],self.vertices[2],self.vertices[7],self.vertices[6]]),
+            Face([self.vertices[6],self.vertices[7],self.vertices[5],self.vertices[4]]),
+            Face([self.vertices[4],self.vertices[5],self.vertices[1],self.vertices[0]]),
+            Face([self.vertices[1],self.vertices[5],self.vertices[7],self.vertices[2]]),
+            Face([self.vertices[4],self.vertices[0],self.vertices[3],self.vertices[6]])
         ]
-    def checkFaceHit(self,face,ray:Ray) -> Collision:
-        normal = self.calculateNormal(face)
-        d = (face[0]-ray)*normal/(normal*ray.direction)
-        intersection = ray.direction*d+ray.position
-        hit = self.inBounds(intersection,face)
-        collision = Collision(intersection if hit else None,hit)
-        return collision
+
     def checkHit(self,ray:Ray) -> Collision:
         collisions = []
         for face in self.faces:
-            collision = self.checkFaceHit(face,ray)
+            collision = face.checkFaceHit(ray)
             if not collision.hit:
                 continue
             collisions.append([collision,ray.lengthFromOrigin(collision.hitPos)])
         collisions = sorted(collisions,key=lambda x:x[1])
         return collisions[0][0]
 
-    def calculateNormal(self,face):
-        vec:Vector3 = face[0]@face[1]
-        return vec.normalize()
-    def inBounds(self,point,face):
-        result = (point-face[0])*self.calculateNormal(face)
-        return True if result == 0 else False
---
+++ easygame/Renderer/Face.py
@@ -0,0 +1,25 @@
+from easygame.Collider.AbstractCollider import Collision
+from easygame.Renderer.Raycaster import Ray
+from easygame.vector import Vector3
+
+
+class Face:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.normal = self.calculateNormal()
+
+    def calculateNormal(self):
+        vec: Vector3 = self.vertices[0] @ self.vertices[1]
+        return vec.normalize()
+
+    def checkFaceHit(self, ray: Ray) -> Collision:
+        normal = self.normal
+        d = (self.vertices[0] - ray) * normal / (normal * ray.direction)
+        intersection = ray.direction * d + ray.position
+        hit = self.inBounds(intersection)
+        collision = Collision(intersection if hit else None, hit)
+        return collision
+
+    def inBounds(self, point:Vector3):
+        result = (point - self.vertices) * self.calculateNormal()
+        return True if result == 0 else False
